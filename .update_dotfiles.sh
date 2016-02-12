4d3151b6b018e423fea0aaa4eab840871e9a641f
robruma <robruma@gmail.com>
2016-02-11 20:12:09 -0500

    Fixing comment

--- .update_dotfiles.sh
+++ .update_dotfiles.sh
@@ -2,6 +2,6 @@
 git pull origin master 
 git submodule init 
 git submodule foreach --recursive git pull origin master
-git commit -a -m "Updating modules"
+git commit -a -m "Updating dotfiles"
 git push origin master
 git submodule update
