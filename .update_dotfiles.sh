80a313c96f699b6021844df6e02dfd7f2d9fd838
robruma <robruma@gmail.com>
2016-02-13 10:09:53 -0500

    Updating script

--- .update_dotfiles.sh
+++ .update_dotfiles.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+if [[ $# -gt 0 ]]; then
+  COMMENT=${@}
+else
+  COMMENT="Updating dotfiles"
+fi
 git pull origin master 
 git submodule init 
 git submodule foreach --recursive git pull origin master
-git commit -a -m "Updating dotfiles"
+git commit -a -m "${COMMENT}"
 git push origin master
 git submodule update
