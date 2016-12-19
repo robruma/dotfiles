1575856bcb9d71338b5b182b83a09bf4014345e9
Rob Ruma <robruma@gmail.com>
2016-12-19 14:20:17 -0500

    Updating .update_dotfiles.sh

--- .update_dotfiles.sh
+++ .update_dotfiles.sh
@@ -3,14 +3,19 @@
 # This will use all arguments to the script as the commit message
 COMMENT=${@:-"Updating dotfiles"}
 
+# What branch are we using?
+BRANCH=$(git symbolic-ref -q HEAD)
+BRANCH=${BRANCH##refs/heads/}
+BRANCH=${BRANCH:-HEAD}
+
 # Conditionally run git commands to properly update all submodules within this repo
 git fetch
-if [[ -n $(git status --porcelain) ]] || [[ $(git diff --exit-code --quiet origin/master) ]]; then
-  git pull origin master 
+if [[ -n $(git status --porcelain) ]] || [[ $(git diff --exit-code --quiet origin/${BRANCH}) ]]; then
+  git pull origin ${BRANCH} 
   git submodule init 
-  git submodule foreach --recursive git pull origin master
+  git submodule foreach --recursive git pull origin ${BRANCH}
   git commit -a -m "${COMMENT}"
-  git push origin master
+  git push origin ${BRANCH}
   git submodule update
 else
   echo "Nothing to update"
