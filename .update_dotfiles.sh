648e1a7b63feaf2a7769f40cbb47219f03555ef1
Rob Ruma <robruma@gmail.com>
2017-01-06 08:08:40 -0500

    Fix backspace functionality and optimize update script

--- .update_dotfiles.sh
+++ .update_dotfiles.sh
@@ -1,31 +1,23 @@
 #!/bin/bash
 # Setting COMMENT with a default value
-# This will use all arguments to the script as the commit message
+# Use all arguments to this script as the commit message
 COMMENT=${@:-"Updating dotfiles"}
 
-# What branch are we using?
+# Which branch are we using?
 BRANCH=$(git symbolic-ref -q HEAD)
 BRANCH=${BRANCH##refs/heads/}
 BRANCH=${BRANCH:-HEAD}
 
-# Conditionally run git commands to properly update all submodules within this repo
+# Fetch changes then check remote and local status to conditionally update all submodules
 git fetch
 
-update_checkout() {
+if [[ $(git diff --exit-code --quiet origin/${BRANCH}) ]] || [[ -n $(git status --porcelain) ]]; then
   git pull origin ${BRANCH} 
   git submodule init 
   git submodule foreach --recursive git pull origin ${BRANCH}
   git commit -a -m "${COMMENT}"
   git push origin ${BRANCH}
   git submodule update
-  exit $?
-}
-
-if [[ -n $(git status --porcelain) ]]; then
-  update_checkout
-  if [[ $(git diff --exit-code --quiet origin/${BRANCH}) ]]; then
-    update_checkout
-  fi
 else
   echo "Nothing to update"
 fi
